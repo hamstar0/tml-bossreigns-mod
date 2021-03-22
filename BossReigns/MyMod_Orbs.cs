aa1f61210893c07948aee3dbe74cf63ed0be76a0
hamstar0 <webutek@gmail.com>
2021-03-22 07:19:33 -0700

    v1.2.1 - Fixed interminal boss reign after PKE resolved

--- BossReigns/MyMod_Orbs.cs
+++ BossReigns/MyMod_Orbs.cs
@@ -13,36 +13,33 @@ namespace BossReigns {
 		public static void ApplyOrbsBossReignEffects() {
 			var config = BossReignsConfig.Instance;
 			if( !config.Get<bool>( nameof(config.BlockOrbUseDuringReign) ) ) {
+				BossReignsMod.UnapplyOrbsBossReignEffects();
+
 				return;
 			}
 
 			var orbConfig = Orbs.OrbsConfig.Instance;
-
-			BossReignsMod._OldEnableOrbUseUponTiles = orbConfig.Get<bool>( nameof(orbConfig.EnableOrbUseUponTiles) );
-			BossReignsMod._OldOrbDisabledMessage = orbConfig.Get<string>( nameof(orbConfig.OrbDisabledMessage) );
+			
+			if( !BossReignsMod._OldEnableOrbUseUponTiles.HasValue ) {
+				BossReignsMod._OldEnableOrbUseUponTiles = orbConfig.Get<bool>( nameof(orbConfig.EnableOrbUseUponTiles) );
+				BossReignsMod._OldOrbDisabledMessage = orbConfig.Get<string>( nameof(orbConfig.OrbDisabledMessage) );
+			}
 
 			orbConfig.SetOverride<bool>( "EnableOrbUseUponTiles", false );
 			orbConfig.SetOverride<string>( "OrbDisabledMessage", "PKE interference disrupts orb use." );
 		}
 
 		public static void UnapplyOrbsBossReignEffects() {
-			var config = BossReignsConfig.Instance;
-			bool noOrbBlocking = config.Get<bool>( nameof( config.BlockOrbUseDuringReign ) );
-
 			var orbConfig = Orbs.OrbsConfig.Instance;
 
 			if( BossReignsMod._OldEnableOrbUseUponTiles.HasValue ) {
 				orbConfig.SetOverride<bool>( "EnableOrbUseUponTiles", BossReignsMod._OldEnableOrbUseUponTiles.Value );
 				BossReignsMod._OldEnableOrbUseUponTiles = null;
-			} else if( !noOrbBlocking ) {
-				orbConfig.UnsetOverride<bool>( "EnableOrbUseUponTiles" );
 			}
 
 			if( BossReignsMod._OldOrbDisabledMessage != null ) {
 				orbConfig.SetOverride<string>( "OrbDisabledMessage", BossReignsMod._OldOrbDisabledMessage );
 				BossReignsMod._OldOrbDisabledMessage = null;
-			} else if( !noOrbBlocking ) {
-				orbConfig.UnsetOverride<string>( "OrbDisabledMessage" );
 			}
 		}
 	}
