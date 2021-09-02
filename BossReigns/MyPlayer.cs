ba8fda1fb94a79df9f0ab2637410346e169b935e
hamstar0 <webutek@gmail.com>
2021-09-02 09:49:19 -0700

    v1.3.0 - Increased duration between quakes

--- BossReigns/MyPlayer.cs
+++ BossReigns/MyPlayer.cs
@@ -1,8 +1,6 @@
-using Microsoft.Xna.Framework;
 using Terraria;
 using Terraria.ModLoader;
 using ModLibsCore.Libraries.Debug;
-using ModLibsCamera.Classes.CameraAnimation;
 
 
 namespace BossReigns {
@@ -29,63 +27,6 @@ namespace BossReigns {
 			this.UpdateReignPerTick( isReign );
 
 			this.IsReignSinceLastTick = isReign;
-		}
-
-
-		////////////////
-
-		private void UpdateReignPerTick( bool isReign ) {
-			if( isReign ) {
-				if( this.player.townNPCs == 0 ) {
-					if( this.player.chest >= 0 ) {
-						this.player.chest = -1;
-
-						Main.NewText( "Dark forces of the land seal this chest.", Color.Yellow );
-					}
-				}
-
-				if( BossReignsMod.Instance.NecrotisMod != null ) {
-					BossReignsPlayer.UpdateReignForNecrotis_WeakRef( this.player );
-				}
-
-				this.UpdateReignFx();
-			}
-		}
-
-		private void UpdateReignPer5Ticks( bool isReign ) {
-			if( isReign ) {
-				if( !this.IsReignSinceLastTick ) {
-					Main.NewText( "A fell presence can be felt. A powerful, unconquered entity now reigns in your world.", Color.OrangeRed );
-				}
-			} else {
-				if( this.IsReignSinceLastTick ) {
-					Main.NewText( "The fell presence has vanished... for now.", new Color(175, 75, 255) );
-				}
-			}
-
-			if( ModLoader.GetMod("CursedBrambles") != null ) {
-				BossReignsPlayer.UpdateReignForCursedBrambles_WeakRef( this.player, isReign );
-			}
-		}
-
-
-		////////////////
-
-		private void UpdateReignFx() {
-			if( this.ShakeIntermissionTimer-- <= 0 ) {
-				this.ShakeIntermissionTimer = Main.rand.Next( 60 * 5, 60 * 20 );
-
-				if( CameraShaker.Current == null || !CameraShaker.Current.IsAnimating() ) {
-					CameraShaker.Current = new CameraShaker(
-						name: "BossReignQuakes",
-						peakMagnitude: 0.5f + Main.rand.NextFloat(),
-						toDuration: 60,
-						lingerDuration: 60,
-						froDuration: 60,
-						isSmoothed: true
-					);
-				}
-			}
 		}
 	}
 }
---
+++ BossReigns/MyPlayer_Reign.cs
@@ -0,0 +1,64 @@
+using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ModLoader;
+using ModLibsCore.Libraries.Debug;
+using ModLibsCamera.Classes.CameraAnimation;
+
+
+namespace BossReigns {
+	partial class BossReignsPlayer : ModPlayer {
+		private void UpdateReignPerTick( bool isReign ) {
+			if( isReign ) {
+				if( this.player.townNPCs == 0 ) {
+					if( this.player.chest >= 0 ) {
+						this.player.chest = -1;
+
+						Main.NewText( "Dark forces of the land seal this chest.", Color.Yellow );
+					}
+				}
+
+				if( BossReignsMod.Instance.NecrotisMod != null ) {
+					BossReignsPlayer.UpdateReignForNecrotis_WeakRef( this.player );
+				}
+
+				this.UpdateReignFx();
+			}
+		}
+
+		private void UpdateReignPer5Ticks( bool isReign ) {
+			if( isReign ) {
+				if( !this.IsReignSinceLastTick ) {
+					Main.NewText( "A fell presence can be felt. A powerful, unconquered entity now reigns in your world.", Color.OrangeRed );
+				}
+			} else {
+				if( this.IsReignSinceLastTick ) {
+					Main.NewText( "The fell presence has vanished... for now.", new Color(175, 75, 255) );
+				}
+			}
+
+			if( ModLoader.GetMod("CursedBrambles") != null ) {
+				BossReignsPlayer.UpdateReignForCursedBrambles_WeakRef( this.player, isReign );
+			}
+		}
+
+
+		////////////////
+
+		private void UpdateReignFx() {
+			if( this.ShakeIntermissionTimer-- <= 0 ) {
+				this.ShakeIntermissionTimer = Main.rand.Next( 60 * 15, 60 * 60 );
+
+				if( CameraShaker.Current == null || !CameraShaker.Current.IsAnimating() ) {
+					CameraShaker.Current = new CameraShaker(
+						name: "BossReignQuakes",
+						peakMagnitude: 0.5f + Main.rand.NextFloat(),
+						toDuration: 60,
+						lingerDuration: 60,
+						froDuration: 60,
+						isSmoothed: true
+					);
+				}
+			}
+		}
+	}
+}
