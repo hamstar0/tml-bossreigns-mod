ec544ea2606eb5d02e0e921428dfafb94f5b2926
hamstar0 <webutek@gmail.com>
2022-05-18 16:26:03 -0700

    v1.4.0.3 - Tweaked PKE priority

--- BossReigns/MyMod_PKE.cs
+++ BossReigns/MyMod_PKE.cs
@@ -38,7 +38,7 @@ namespace BossReigns {
 				return new PKEMeter.Logic.PKETextMessage(
 					message: "WARNING - CLASS V+ PKE-EMITTING ENTITIES AT LARGE",
 					color: Color.Red * (0.5f + ( Main.rand.NextFloat() * 0.5f) ),
-					priority: gauges.RedRealPercent >= 0.99f ? 1f : 0f
+					priority: gauges.RedRealPercent >= 0.99f ? 1f : float.Epsilon
 				);
 			} );
 
